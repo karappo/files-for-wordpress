ad0b0afd6aff5657d848d541fdc3acb708c001bb
Naokazu Terada <naokazu.terada@gmail.com>
2025-05-26 16:13:07 +0900

    バーの項目調整

--- project-specific.php
+++ project-specific.php
@@ -49,9 +49,10 @@ add_action( 'admin_menu', 'custom_admin_menu' );
 
 function my_remove_adminbar_menu($wp_admin_bar) {
   if(!is_karappo_admin()) {
-    // $wp_admin_bar->remove_menu('wp-logo');      // WPロゴ
+    $wp_admin_bar->remove_menu('wp-logo');      // WPロゴ
     // $wp_admin_bar->remove_menu('site-name');    // サイト名
-    // $wp_admin_bar->remove_menu('view-site');    // サイト名 -> サイトを表示
+    $wp_admin_bar->remove_menu('view-site');    // サイト名 -> サイトを表示
+    $wp_admin_bar->remove_menu('plugins');      // サイト名 -> プラグイン
     $wp_admin_bar->remove_menu('dashboard');    // サイト名 -> ダッシュボード (公開側)
     $wp_admin_bar->remove_menu('themes');       // サイト名 -> テーマ (公開側)
     $wp_admin_bar->remove_menu('customize');    // サイト名 -> カスタマイズ (公開側)
@@ -64,10 +65,10 @@ function my_remove_adminbar_menu($wp_admin_bar) {
     $wp_admin_bar->remove_menu('new-link');     // 新規 -> リンク
     $wp_admin_bar->remove_menu('new-page');     // 新規 -> 固定ページ
     $wp_admin_bar->remove_menu('new-user');     // 新規 -> ユーザー
-    $wp_admin_bar->remove_menu('my-account');   // マイアカウント
-    $wp_admin_bar->remove_menu('user-info');    // マイアカウント -> プロフィール
-    $wp_admin_bar->remove_menu('edit-profile'); // マイアカウント -> プロフィール編集
-    $wp_admin_bar->remove_menu('logout');       // マイアカウント -> ログアウト
+    // $wp_admin_bar->remove_menu('my-account');   // マイアカウント
+    // $wp_admin_bar->remove_menu('user-info');    // マイアカウント -> プロフィール
+    // $wp_admin_bar->remove_menu('edit-profile'); // マイアカウント -> プロフィール編集
+    // $wp_admin_bar->remove_menu('logout');       // マイアカウント -> ログアウト
     $wp_admin_bar->remove_menu('search');       // 検索 (公開側)
   }
 }
